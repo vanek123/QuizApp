94aeaacb143e0ffeba5ab0b40d06a8488c326e47
vanek123 <vanjaaleskevic@mail.ru>
2023-08-01 19:26:44 +0300

    question/score display

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -48,12 +50,13 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-
     if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
         //go to the end page
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -76,9 +79,13 @@ choices.forEach(choice => {
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
         
-        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-        console.log(classToApply);
+        const classToApply =
+        selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+        if(classToApply == 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+        
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout( () => {
@@ -88,5 +95,10 @@ choices.forEach(choice => {
        
     })
 })
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
 
 startGame();
