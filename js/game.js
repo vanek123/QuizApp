6d5a65981fa9b3634500f7f1bb7d6227aac648b5
vanek123 <vanjaaleskevic@mail.ru>
2023-08-01 19:46:23 +0300

    progress bar

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -55,7 +56,9 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
@@ -85,7 +88,7 @@ choices.forEach(choice => {
         if(classToApply == 'correct') {
             incrementScore(CORRECT_BONUS);
         }
-        
+
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout( () => {
