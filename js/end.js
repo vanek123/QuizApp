2e4f83732e4ae696fe6365674fbd22779036ea6f
vanek123 <vanjaaleskevic@mail.ru>
2023-08-02 10:12:52 +0300

    save high scores

--- js/end.js
+++ js/end.js
@@ -5,9 +5,10 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
 
-username.addEventListener('keyup', () => {
+const MAX_HIGH_SCORES = 5;
+
+username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
 
@@ -15,9 +16,14 @@ saveHighScore = (e) => {
     e.preventDefault();
 
     const score = {
-        score: mostRecentScore,
+        score: Math.floor(Math.random() * 100),
         name: username.value
     };
     highScores.push(score);
-    console.log(highScores);
+    highScores.sort( (a,b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
+
 };
